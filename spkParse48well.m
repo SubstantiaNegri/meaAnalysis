eb4d1fbd146c2710ffa5d1c543734c1eec85636e
Joe Negri <jnegri@g.harvard.edu>
2019-01-02 10:46:47 -0500

    Added Illustrator version of meaAnalysisFlowChart.ai and Matlab scripts spkParse48well.m and spkParse96well.m to git tracking

---
+++ spkParse48well.m
@@ -0,0 +1,45 @@
+path('~/matlab/axion/',path);
+spk_files = dir('*.spk' );
+for f = 1:length(spk_files)
+    SpkRec = spk_files(f).name;
+    SpkRecString = split(spk_files(f).name,[string('_'),string('Rec-')]);
+    SpkRecData = AxisFile(SpkRec).DataSets.LoadData;
+    for r = 1:6
+        for c = 1:8
+           for x = 1:4
+             for y = 1:4
+              try
+                 arrayVolt = []; %clear arrayVolt to free mem for sucessive loops
+	  	 arrayTime = [];
+		 cellSD = [];
+		 vectorSD = [];
+		 arrayVolt = SpkRecData{r,c,x,y}.GetVoltageVector;
+		 arrayTime = SpkRecData{r,c,x,y}.GetTimeVector;
+                 cellSD = {SpkRecData{r,c,x,y}.StandardDeviation};
+                 vectorSD = cell2mat(cellSD);
+                 catch ME
+                    if (strcmp(ME.identifier,'MATLAB:structRefFromNonStruct'))
+                            continue
+                    end
+              end
+              plate = SpkRecString(3);
+              rec = SpkRecString(9);
+              row = num2str(r);
+              column = num2str(c);
+              array_x = num2str(x);
+              array_y = num2str(y);
+              filenameVolt = strcat(plate,'_',rec,'_',row,'_',column,...
+                  '_',array_x,'_',array_y,'_','volt','.csv');
+	      csvwrite(char(filenameVolt),arrayVolt);
+	      filenameTime = strcat(plate,'_',rec,'_',row,'_',column,...
+		  '_',array_x,'_',array_y,'_','time','.csv');
+	      csvwrite(char(filenameTime),arrayTime(1,:));
+              filenameSD = strcat(plate,'_',rec,'_',row,'_',column,...
+                  '_',array_x,'_',array_y','_','SD','.csv');
+              csvwrite(char(filenameSD),vectorSD);
+             end
+           end
+        end
+    end
+end   
+exit    
